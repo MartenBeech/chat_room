7f06e1129c5e47e8269894ec569459ef3a4c7406
MartenBeech <mortenbech22@hotmail.com>
2023-02-03 09:40:46 +0100

    Updated google login button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {onGoogleButtonPress} from '../firebase/auth';
+import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
 
 export const LoginPage = ({navigation}) => {
   return (
-    <View>
-      <Button
-        title="Go to lobby"
+    <View style={styles.container}>
+      <GoogleSigninButton
+        style={styles.googleButton}
         onPress={() =>
-          onGoogleButtonPress().then(() => navigation.navigate('LobbyPage'))
+          onGoogleButtonPress().then(result => {
+            if (result) {
+              navigation.navigate('LobbyPage');
+            }
+          })
         }
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  googleButton: {
+    width: '60%',
+    marginTop: 40,
+  },
+});
